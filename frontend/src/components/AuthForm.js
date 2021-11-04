11eff7dfa117beb20b2b78fef70725aaef26ef52
Pete VS <pete_vs88@me.com>
2021-11-04 12:14:08 -0600

    added useForm to authform

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -4,27 +4,45 @@ import FormHeader from './form/FormHeader'
 import FormFooter from './form/FormFooter'
 import FormSubmit from './form/FormSubmit'
 import ForgotPassword from './form/ForgotPassword'
+import useForm from 'hooks/useForm'
 
 const AuthForm = () => {
+
+    const initialFormValues = {
+        email: '',
+        password: ''
+    }
+
+    const {values, handleFormChange } = useForm(initialFormValues)
+
+    const inputSize= 'medium'
+
+
     return (
         <FormBox>
             <FormHeader heading='Log in' />
 
             <TextField
                 label='Email Address'
+                name='email'
                 id='email'
-                size='medium'
+                size={inputSize}
                 placeholder='example@mail.com'
+                value={values.email}
+                onChange={handleFormChange}
             />
             <TextField
                 label='Password'
+                name='password'
                 id='password'
                 type='password'
-                size='medium'
+                size={inputSize}
                 placeholder='At least 8 characters'
+                value={values.password}
+                onChange={handleFormChange}
             />
 
-                <FormSubmit>Log in</FormSubmit>
+                <FormSubmit onClick={() => console.log(values)}>Log in</FormSubmit>
                 <ForgotPassword />
                 <FormFooter
                     heading="Don't have an account?"
