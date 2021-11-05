b768538cdea0b53941d94564e9e831932f7b0817
Pete VS <pete_vs88@me.com>
2021-11-05 14:27:33 -0600

    avatar button on nav now dynamic image

--- frontend/src/components/userMenu/components/AvatarButton.js
+++ frontend/src/components/userMenu/components/AvatarButton.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import { useContext } from 'react'
 import { IconButton, Avatar } from '@mui/material'
+import GlobalContext from 'state/GlobalContext'
 
 const AvatarButton = ({handleClick, src}) => {
+
+    const { state } = useContext(GlobalContext)
+    const { avatarURL } = state.user
+
     return (
         <IconButton onClick={handleClick}>
             <Avatar
                 sx={{ width: 32, height: 32 }}
-                src={src || ''}
+                src={src || avatarURL}
             />
         </IconButton>
     )
