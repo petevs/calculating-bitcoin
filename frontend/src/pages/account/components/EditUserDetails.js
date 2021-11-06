6da13c3e8cdc21827969aa0f67b0263d6f05fea9
Pete VS <pete_vs88@me.com>
2021-11-05 22:31:23 -0600

    added more styling to account form

--- frontend/src/pages/account/components/EditUserDetails.js
+++ frontend/src/pages/account/components/EditUserDetails.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import useForm from 'hooks/useForm'
 import GlobalContext from 'state/GlobalContext'
-import { TextField } from '@mui/material'
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import { Box } from '@mui/system'
 
 const EditUserDetails = () => {
@@ -18,6 +18,12 @@ const EditUserDetails = () => {
     const {values, handleFormChange} = useForm(initialFormValues)
 
     console.log(initialFormValues, values)
+
+    const currencies = [
+        ['usd', 'USD - United States Dollar'],
+        ['cad', 'CAD - Canadian Dollar']
+    ]
+
 
     return (
         <Box sx={{
@@ -39,14 +45,35 @@ const EditUserDetails = () => {
                 value={values.displayName}
                 onChange={(e) => handleFormChange(e)}
             />
-            <TextField
-                id='currency'
-                name='currency'
-                label='Currency'
-                type='select'
-                value={values.currency}
-                onChange={(e) => handleFormChange(e)}
-            />
+            <FormControl>
+                <InputLabel id='currency'>Default Currency</InputLabel>
+                <Select
+                    id='currency'
+                    name='currency'
+                    label='Default Currency'
+                    type='select'
+                    value={values.currency}
+                    onChange={(e) => handleFormChange(e)}
+                >
+                    {
+                        currencies.map(currency => 
+                            <MenuItem
+                                value={currency[0]}
+                            >
+                                {currency[1]}
+                            </MenuItem>
+                                )
+                    }
+                </Select>
+            </FormControl>
+            <Button
+                variant='contained'
+                color='info'
+                sx={{
+                    gridColumn: '1 / span 2', 
+                    justifySelf: 'end'
+                }}
+            >Save Changes</Button>
         </Box>
     )
 }
