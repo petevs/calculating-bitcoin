cc83f895cbd093b899ceb5cc2e5ff6206a9b2abb
Pete VS <pete_vs88@me.com>
2021-11-17 10:40:36 -0700

    updated portfolio chart

--- frontend/src/pages/portfolio/components/PortfolioChart.js
+++ frontend/src/pages/portfolio/components/PortfolioChart.js
@@ -1,34 +1,56 @@
 import { Box } from "@mui/system"
 import Chart from 'react-apexcharts'
-import { Typography, useMediaQuery } from '@mui/material'
+import { Typography, useMediaQuery, TextField, MenuItem, InputAdornment, Divider } from '@mui/material'
 import NumberFormat from 'react-number-format'
-import { Button } from "@mui/material"
 import { renderToString } from 'react-dom/server'
+import { useState } from 'react'
 
 const PortfolioChart = ({ data }) => {
 
-    console.log(data)
+    const [chartType, setChartType] = useState('Portfolio Value')
+    
+    const handleChartTypeChange = (e) => {
+        setChartType(e.target.value)
+    }
+    
 
     const mobile = useMediaQuery('(min-width:1024px')
 
     const categories = data.map(item => item.date).reverse()
 
-    const series = [{
-        name: `Portfolio Value`,
-        data: data.map((item) => {
-            return {
-                x: item.date,
-                y: Math.round(item.currentValue),
-                price: Math.round(item.historicalPrice),
-            }
-        }).reverse()
-    }]
+    const series = [
+        {
+            name: `Portfolio Value`,
+            data: data.map((item) => {
+                return {
+                    x: item.date,
+                    y: Math.round(item.currentValue),
+                    price: Math.round(item.historicalPrice),
+                    bitcoinHoldings: item.runningBitcoinBalance
+                }
+            }).reverse()
+        },
+        {
+            name: `Total Invested`,
+            data: data.map((item) => {
+                return {
+                    x: item.date,
+                    y: Math.round(item.totalDeposits),
+                }
+            }).reverse()
+        },
+
+]
 
     const CustomTooltip = (props) => {
         return (
             <Box sx={{padding: '1rem', display: 'grid', gridTemplateColumns: '1fr', alignItems: 'start'}}>
+                <Box sx={{borderBottom: '1px solid rgba(255, 255, 255, 0.12)', paddingBottom: '.5rem', marginBottom: '.5rem'}}>
+                    <Typography variant='body2'>{props.x}</Typography>
+                </Box>
                 <Typography variant='body1'>Portfolio Value: <NumberFormat displayType='text' value={props.y} prefix='$' thousandSeparator={true} /></Typography>
                 <Typography variant='body1'>Price: <NumberFormat displayType='text' value={props.price} prefix='$' thousandSeparator={true} /></Typography>
+                <Typography variant='body1'>Bitcoin Holdings: <NumberFormat displayType='text' value={props.bitcoinHoldings} decimalScale={8} fixedDecimalScale={8} /></Typography>
             </Box>
         )
     }
@@ -117,14 +139,33 @@ const PortfolioChart = ({ data }) => {
 
 
     return (
-        <Box sx={wrapper}>
-            <Chart
-                series={series}
-                options={options}
-                type='area'
-                width='100%'
-                height="400px"
-            />
+        <Box>
+            <Box>
+                <TextField
+                    select
+                    value={chartType}
+                    onChange={handleChartTypeChange}
+                    size='small'
+                    sx={{width: '350px'}}
+                    InputProps={{
+                        startAdornment: (<InputAdornment position='start'>
+                            Chart Type:
+                        </InputAdornment>),
+                    }}
+                >
+                    <MenuItem value='Portfolio Value'>Portfolio Value</MenuItem>
+                    <MenuItem value='Bitcoin Holdings'>Bitcoin Holdings</MenuItem>
+                </TextField>
+            </Box>
+            <Box sx={wrapper}>
+                <Chart
+                    series={series}
+                    options={options}
+                    type='area'
+                    width='100%'
+                    height="400px"
+                />
+            </Box>
         </Box>
     )
 }
