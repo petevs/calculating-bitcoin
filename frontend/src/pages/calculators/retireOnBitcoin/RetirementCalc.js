a5a3cf2d065d4e874ef79ca20ed1fc61fd6c64e7
Pete VS <pete_vs88@me.com>
2021-11-29 11:52:37 -0700

    setup toggle for calculator drawer

--- frontend/src/pages/calculators/retireOnBitcoin/RetirementCalc.js
+++ frontend/src/pages/calculators/retireOnBitcoin/RetirementCalc.js
@@ -12,6 +12,7 @@ import RetirementChart from './RetirementChart'
 import SavedCalcs from './SavedCalcs'
 import InputSlider from './InputSlider'
 import { Drawer, Typography } from '@mui/material'
+import RetirementDrawer from './RetirementDrawer'
 
 const RetirementCalc = () => {
 
@@ -29,36 +30,12 @@ const RetirementCalc = () => {
 
     return (
         <>
-        <Drawer
-            anchor={'right'}
-            open={true}
-            variant={'persistent'}
-            sx={{
-                '& .MuiDrawer-paper': {
-                    width: '300px',
-                    top: '75px',
-                    right: '10px',
-                    backgroundColor: 'rgb(33, 43, 54)',
-                    borderRadius: '8px',
-                    backgroundImage: 'none',
-                    color: 'rgb(145,158,171)',
-                    borderRight: '1px solid rgba(145, 158, 171, 0.24)',
-                    boxShadow: 'rgb(0 0 0 / 24%) 0px 0px 4px 0px, rgb(0 0 0 / 24%) 0px 24px 48px 0px',
-                    height: 'calc(100% - 175px)'
-                }
-                }}
-                BackdropProps={{ invisible: true }}
-        >
-
-            <Typography sx={{fontSize: '1rem', fontWeight: '700', color: '#fff', padding: '1rem', borderBottom: '1px solid rgba(145, 158, 171, 0.24)'}}>Edit Details</Typography>
-            <RetirementForm state={reducerState} dispatch={dispatch} updateValue={updateValue}/>
-        </Drawer>
+        <RetirementDrawer state={reducerState} dispatch={dispatch} updateValue={updateValue} />
         <Page sx={{justifyContent: 'stretch', alignContent: 'start', gap: '1rem'}}>
             <PageHeader title='Retire on Bitcoin Calculator' />
             <RetirementSummary state={reducerState} />
             <Box sx={{display: 'grid', gridTemplateColumns: '1fr', alignItems: 'start'}}>
                 <RetirementChart data={reducerState.resultsTable()} />
-                {/* <RetirementForm state={reducerState} dispatch={dispatch} updateValue={updateValue}/> */}
             </Box>
             <RetirementCalcTabs
                 tabs={[
---
+++ frontend/src/pages/calculators/retireOnBitcoin/RetirementDrawer.js
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+import { Drawer, IconButton, Typography } from '@mui/material'
+import RetirementForm from './RetirementForm'
+import { Box } from '@mui/system'
+import CloseIcon from '@mui/icons-material/Close';
+import EditIcon from '@mui/icons-material/Edit';
+
+const RetirementDrawer = ({state, dispatch, updateValue}) => {
+
+    const [open, setOpen] = useState(true)
+
+    const toggleDrawer = () => {
+        setOpen(!open)
+    }
+
+
+    return (
+        <>
+        <Box
+            sx={{
+                position: 'fixed',
+                top: '50%',
+                right: open ? '10px' : '0px',
+                backgroundColor: 'rgb(33, 43, 54)',
+                boxShadow: 'rgb(0 0 0 / 24%) 0px 0px 4px 0px, rgb(0 0 0 / 24%) 0px 24px 48px 0px',
+                borderRadius: '20px 0 0 20px',
+                padding: '.5rem',
+                transition: 'transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms'
+            }}
+        >
+            <IconButton 
+                size='large'
+                onClick={toggleDrawer}
+            >
+                <EditIcon />
+            </IconButton>
+
+        </Box>
+        <Drawer
+            anchor={'right'}
+            open={open}
+            variant={'persistent'}
+            sx={{
+                '& .MuiDrawer-paper': {
+                    width: '300px',
+                    top: '75px',
+                    right: '10px',
+                    backgroundColor: 'rgb(33, 43, 54)',
+                    borderRadius: '8px',
+                    backgroundImage: 'none',
+                    color: 'rgb(145,158,171)',
+                    borderRight: '1px solid rgba(145, 158, 171, 0.24)',
+                    boxShadow: 'rgb(0 0 0 / 24%) 0px 0px 4px 0px, rgb(0 0 0 / 24%) 0px 24px 48px 0px',
+                    height: 'calc(100% - 175px)'
+                }
+                }}
+                BackdropProps={{ invisible: true }}
+        >
+            <Box
+                sx={{
+                    display: 'grid',
+                    gridTemplateColumns: '1fr auto',
+                    alignItems: 'center',
+                    fontSize: '1rem', 
+                    fontWeight: '700', 
+                    color: '#fff', 
+                    padding: '1rem', 
+                    borderBottom: '1px solid rgba(145, 158, 171, 0.24)'
+                }}
+            >
+                <Typography 
+                >
+                    Edit Details
+                </Typography>
+                <IconButton 
+                    size='small'
+                    onClick={toggleDrawer} 
+                    sx={{
+                        '& svg': {
+                            height: '1rem', 
+                            width: '1rem'
+                        }}}>
+                    <CloseIcon />
+                </IconButton>
+            </Box>
+            <RetirementForm state={state} dispatch={dispatch} updateValue={updateValue}/>
+        </Drawer>
+        </>
+    )
+}
+
+export default RetirementDrawer
